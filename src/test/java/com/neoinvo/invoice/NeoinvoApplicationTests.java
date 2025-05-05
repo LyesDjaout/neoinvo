437d5863fe1c36d42f7ba39ae5fdc009cb0e146b
djaout lyes <djaoutlyes24@gmail.com>
2025-05-05 20:34:07 +0200

    init: create base structure for Neoinvo PDP

---
+++ src/test/java/com/neoinvo/invoice/NeoinvoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoinvo.invoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoinvoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
